50e47ee4e8ac13f5ea37f4d1024fbd06ca4e5197
Sarp Uner <su26@duke.edu>
2018-03-21 05:02:16 -0400

    Finished final dictionary implementation

--- read_csv.py
+++ read_csv.py
@@ -1,10 +1,14 @@
 import csv, os, sys
 from operator import itemgetter
 from numpy import median
+
 def create_dict():
     my_dict = {"10": dict(), "20": dict(), "30": dict(), "40": dict(), "50": dict(), "60": dict()}
     return my_dict
 
+def create_year_dict():
+    my_dict = {"early": dict(), "late": dict()}
+    return my_dict
 
 def height_extract(my_col):
     #print my_col
@@ -25,6 +29,17 @@ def top_twenty(rows):
                 my_dict[row[0]] = {"freq": 1, "rows":[row]}
     return my_dict
 
+def art_year(year):
+    if year:
+        year = year.replace("Circa", "")
+        year = year.replace("Before", "")
+        if "-" in year:
+            year = year.split("-")[1].strip()
+        year = year.replace(" ","")
+
+        return year
+    return 0
+
 def year_range(years):
     my_list = []
     for year in years:
@@ -38,7 +53,7 @@ def year_range(years):
 
             my_list.append(int(year))
     my_list = sorted(my_list)
-    print my_list
+    #print my_list
     my_median = median(my_list)
     return int(my_median)
 
@@ -58,14 +73,23 @@ with open(os.path.join(sys.path[0], 'Sothebys.csv'), 'rU') as csvfile:
     for artist in twenty:
         print artist[0]
         my_year = year_range([row[4] for row in my_dict[artist[0]]["rows"]])
-        if row[0] in artist_dict:
-            my_height=height_extract(row[5])
-            #print my_height
-            if my_height > 0:
-                for h in sorted(artist_dict[row[0]].keys()):
-                    if my_height <= int(h):
-                        continue
-        else:
-            artist_dict[row[0]] = create_dict()
+        #print my_year
+        for row in my_dict[artist[0]]["rows"]:
+            if row[0] in artist_dict:
+                my_height=height_extract(row[5])
+                if my_height > 0:
+                    for h in sorted(artist_dict[row[0]].keys()):
+                        if my_height <= int(h):
+# if year is non-existent, we take it as zero, this needs to be fixed.
+                            if int(art_year(row[4])) <= my_year:
+                                #print art_year(row[4])
+                                artist_dict[row[0]][h]["early"][row[1]] = {"price": row[14], "sale": row[9]}
+                            else:
+                                artist_dict[row[0]][h]["late"][row[1]] = {"price": row[14], "sale": row[9]}
+
+            else:
+                artist_dict[row[0]] = create_dict()
+                for item in artist_dict[row[0]]:
+                    artist_dict[row[0]][item] = create_year_dict()
 
     print artist_dict
