29a2dc50b0174214b34e6703ad7913315d46c1c0
Sarp Uner <su26@duke.edu>
2018-03-21 05:11:00 -0400

    Moved functions around

--- read_csv.py
+++ read_csv.py
@@ -57,39 +57,43 @@ def year_range(years):
     my_median = median(my_list)
     return int(my_median)
 
+def artist_dict():
+    with open(os.path.join(sys.path[0], 'Sothebys.csv'), 'rU') as csvfile:
+        my_file = csv.reader(csvfile, delimiter=',')
+        artist_dict = dict()
 
-with open(os.path.join(sys.path[0], 'Sothebys.csv'), 'rU') as csvfile:
-    my_file = csv.reader(csvfile, delimiter=',')
-    artist_dict = dict()
+        my_dict = top_twenty(my_file)
+        #print my_dict
+        my_list = []
+        for item in my_dict:
+            my_list.append((item, my_dict[item]["freq"]))
 
-    my_dict = top_twenty(my_file)
-    #print my_dict
-    my_list = []
-    for item in my_dict:
-        my_list.append((item, my_dict[item]["freq"]))
+        sorted_list = sorted(my_list, key=itemgetter(1), reverse=True)
+        twenty = sorted_list[:20]
+        for artist in twenty:
+            print artist[0]
+            my_year = year_range([row[4] for row in my_dict[artist[0]]["rows"]])
+            #print my_year
+            for row in my_dict[artist[0]]["rows"]:
+                if row[0] in artist_dict:
+                    my_height=height_extract(row[5])
+                    if my_height > 0:
+                        for h in sorted(artist_dict[row[0]].keys()):
+                            if my_height <= int(h):
+        # if year is non-existent, we take it as zero, this needs to be fixed.
+                                if int(art_year(row[4])) <= my_year:
+                                    #print art_year(row[4])
+                                    artist_dict[row[0]][h]["early"][row[1]] = {"price": row[14], "sale": row[9]}
+                                else:
+                                    artist_dict[row[0]][h]["late"][row[1]] = {"price": row[14], "sale": row[9]}
 
-    sorted_list = sorted(my_list, key=itemgetter(1), reverse=True)
-    twenty = sorted_list[:20]
-    for artist in twenty:
-        print artist[0]
-        my_year = year_range([row[4] for row in my_dict[artist[0]]["rows"]])
-        #print my_year
-        for row in my_dict[artist[0]]["rows"]:
-            if row[0] in artist_dict:
-                my_height=height_extract(row[5])
-                if my_height > 0:
-                    for h in sorted(artist_dict[row[0]].keys()):
-                        if my_height <= int(h):
-# if year is non-existent, we take it as zero, this needs to be fixed.
-                            if int(art_year(row[4])) <= my_year:
-                                #print art_year(row[4])
-                                artist_dict[row[0]][h]["early"][row[1]] = {"price": row[14], "sale": row[9]}
-                            else:
-                                artist_dict[row[0]][h]["late"][row[1]] = {"price": row[14], "sale": row[9]}
+                else:
+                    artist_dict[row[0]] = create_dict()
+                    for item in artist_dict[row[0]]:
+                        artist_dict[row[0]][item] = create_year_dict()
 
-            else:
-                artist_dict[row[0]] = create_dict()
-                for item in artist_dict[row[0]]:
-                    artist_dict[row[0]][item] = create_year_dict()
+        return artist_dict
 
-    print artist_dict
+
+my_dict = artist_dict()
+print my_dict
